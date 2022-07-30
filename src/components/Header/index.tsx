d8667000ed0153a3e90ba32732f042a57f431b43
Dirosaki <diegosgomes27@gmail.com>
2022-07-30 03:46:16 -0300

    Feat: GeoLocation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { MapPin } from 'phosphor-react'
+
+import { formatState } from 'utils/formatState'
 
 import logoImg from 'assets/brand/logo.svg'
 
@@ -8,7 +11,49 @@ import { CartButton } from './components/CartButton'
 
 import * as Styled from './styles'
 
+type ResponseData = {
+	city: string
+	continent: string
+	countryCode: string
+	countryName: string
+	locality: string
+	latitude: number
+	longitude: number
+	principalSubdivision: string
+}
+
 export function Header() {
+	const [myLocation, setMyLocation] = useState('Minha localização')
+
+	const hasGeolocationInNavigator = 'geolocation' in navigator
+
+	const getMyLocation = useCallback(() => {
+		function success(position: GeolocationPosition) {
+			const { latitude } = position.coords
+			const { longitude } = position.coords
+
+			fetch(
+				`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=pt-br`
+			)
+				.then((response) => response.json())
+				.then(({ city, principalSubdivision }: ResponseData) => {
+					const formattedState = formatState(principalSubdivision)
+					setMyLocation(`${city}, ${formattedState}`)
+				})
+		}
+
+		function error() {
+			setMyLocation('Minha localização')
+		}
+
+		navigator.geolocation.getCurrentPosition(success, error)
+	}, [])
+
+	useEffect(() => {
+		if (!hasGeolocationInNavigator) return
+		getMyLocation()
+	}, [getMyLocation, hasGeolocationInNavigator])
+
 	return (
 		<Styled.Header>
 			<div>
@@ -19,10 +64,13 @@ export function Header() {
 					/>
 				</Link>
 				<Styled.ActionsWrapper>
-					<Styled.MyLocation type="button">
-						<MapPin size={22} weight="fill" />
-						<span>Porto Alegre, RS</span>
-					</Styled.MyLocation>
+					{hasGeolocationInNavigator && (
+						<Styled.MyLocation type="button" onClick={getMyLocation}>
+							<MapPin size={22} weight="fill" />
+							<span>{myLocation}</span>
+						</Styled.MyLocation>
+					)}
+
 					<CartButton />
 				</Styled.ActionsWrapper>
 			</div>
