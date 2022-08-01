d80e244204e2fb6524a305654bf771770c2ba865
Dirosaki <diegosgomes27@gmail.com>
2022-07-31 23:53:56 -0300

    Component: CoffeeCardForCart

---
+++ src/pages/Checkout/components/CoffeeCardForCart/index.tsx
@@ -0,0 +1,70 @@
+import { Trash } from 'phosphor-react'
+
+import { useCart } from 'hooks/useCart'
+
+import { CoffeeProps } from 'pages/Home/components/CoffeeCard'
+
+import { NumberInput } from 'components/NumberInput'
+import { Text } from 'components/Typography'
+
+import { formatPrice } from 'utils/formatPrice'
+
+import * as Styled from './styles'
+
+type CoffeeCardForCartProps = {
+	coffee: CoffeeProps
+}
+
+export function CoffeeCardForCart({ coffee }: CoffeeCardForCartProps) {
+	const {
+		removeItemById,
+		increaseItemQuantityById,
+		decreaseItemQuantityById,
+		changeItemQuantityById,
+	} = useCart()
+	const coffeeTotalPrice = coffee.price * coffee.quantity
+
+	const formattedPrice = formatPrice(coffeeTotalPrice)
+
+	function handleRemoveItem() {
+		removeItemById(coffee.id)
+	}
+
+	function handleIncreaseItemQuantity() {
+		increaseItemQuantityById(coffee.id)
+	}
+
+	function handleDecreaseItemQuantity() {
+		decreaseItemQuantityById(coffee.id)
+	}
+
+	function handleChangeItemQuantity(quantity: number) {
+		changeItemQuantityById(coffee.id, quantity)
+	}
+
+	return (
+		<Styled.CardContainer>
+			<Styled.Info>
+				<img src={coffee.img} alt="" />
+				<Styled.Details>
+					<Text color="subtitle" as="strong">
+						{coffee.name}
+					</Text>
+					<NumberInput
+						quantityCoffee={coffee.quantity}
+						onIncreaseQuantityCoffee={handleIncreaseItemQuantity}
+						onDecreaseQuantityCoffee={handleDecreaseItemQuantity}
+						onQuantityCoffeeReceived={handleChangeItemQuantity}
+					/>
+					<Styled.RemoveButton type="button" onClick={handleRemoveItem}>
+						<Trash />
+						<span>Remover</span>
+					</Styled.RemoveButton>
+				</Styled.Details>
+			</Styled.Info>
+			<Text weight={700} as="p">
+				R$ {formattedPrice}
+			</Text>
+		</Styled.CardContainer>
+	)
+}
---
+++ src/pages/Checkout/components/CoffeeCardForCart/styles.ts
@@ -0,0 +1,75 @@
+import styled, { css } from 'styled-components'
+
+export const CardContainer = styled.div`
+	${({ theme }) => css`
+		padding: 8px 4px 32px;
+		display: flex;
+
+		justify-content: space-between;
+		border-bottom: 1px solid ${theme.colors.base.button};
+
+		& + & {
+			margin-top: 24px;
+		}
+
+		p {
+			white-space: nowrap;
+		}
+	`}
+`
+
+export const Info = styled.div`
+	display: flex;
+	gap: 20px;
+
+	img {
+		height: 64px;
+	}
+`
+
+export const Details = styled.div`
+	${({ theme }) => css`
+		padding: 2px 0;
+		display: grid;
+		grid-template-columns: repeat(2, max-content);
+		grid-template-rows: auto 32px;
+		gap: 0 8px;
+
+		strong {
+			grid-column: 1 / 3;
+		}
+	`}
+`
+
+export const RemoveButton = styled.button`
+	${({ theme }) => css`
+		padding: 0 8px;
+		display: flex;
+		align-items: center;
+		gap: 4px;
+		background: ${theme.colors.base.button};
+		border-radius: 6px;
+		border: none;
+		color: ${theme.colors.brand.purple};
+		transition: 0.2s ease-out;
+
+		&:hover {
+			background: ${theme.colors.base.hover};
+
+			span {
+				color: ${theme.colors.base.subtitle};
+			}
+		}
+
+		&:focus-visible {
+			box-shadow: 0 0 0 1px ${theme.colors.brand.purple} inset;
+		}
+
+		span {
+			font-size: 0.75rem;
+			line-height: 1;
+			text-transform: uppercase;
+			color: ${theme.colors.base.text};
+		}
+	`}
+`
